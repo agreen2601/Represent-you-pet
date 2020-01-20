9f3a9d0fccb44be3f64e12061bbdb0042048d949
Andrew Green <green.andrew.b@gmail.com>
2020-01-20 11:22:30 -0600

    added kitty actions

--- represent-your-pet.js
+++ represent-your-pet.js
@@ -1,6 +1,13 @@
-const pet = {
+const kitty = {
     name: "Kitty Baby",
     species: "cat",
-    nicknames: ["kitty baby numba 1", "kitty babu", "the babu cat"]
-    age: 10
+    nicknames: ["kitty baby numba 1", "kitty babu", "the babu cat"],
+    age: 10,
+    herro: window.alert('Kitty Baby hollering "herro-maraaang"'),
+    sleep: window.alert("(Kitty Baby snoring loudly)"),
+    inTheWay: window.alert("Kitty Baby standing direcly in your way as you cook")
 }
+
+kitty.herro()
+kitty.sleep()
+kitty.inTheWay()
