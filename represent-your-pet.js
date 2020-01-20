edfd268448c05b582e93c091a9167a0411bb8a5b
Andrew Green <green.andrew.b@gmail.com>
2020-01-20 11:51:27 -0600

    added some toys to an array if they met a certain condition

--- represent-your-pet.js
+++ represent-your-pet.js
@@ -3,11 +3,30 @@ const kitty = {
     species: "cat",
     nicknames: ["kitty baby numba 1", "kitty babu", "the babu cat"],
     age: 10,
-    herro: window.alert('Kitty Baby hollering "herro-maraaang"'),
-    sleep: window.alert("(Kitty Baby snoring loudly)"),
-    inTheWay: window.alert("Kitty Baby standing direcly in your way as you cook")
+    herro: function () {
+        window.alert('Kitty Baby hollering "herro-maraaang"')
+    },
+    sleep: function () {
+        window.alert("(Kitty Baby snoring loudly)")
+    },
+    inTheWay: function () {
+        window.alert("Kitty Baby standing direcly in your way as you cook")
+    },
+    favoriteToys: [],
+    play: function (toy) {
+        if (toy.includes ("mouse")) {
+            this.favoriteToys.push(toy)
+        }
+    }
 }
 
-kitty.herro()
-kitty.sleep()
-kitty.inTheWay()
+kitty.herro();
+kitty.sleep();
+kitty.inTheWay();
+
+kitty.play("mouse");
+kitty.play("ball");
+kitty.play("toy mouse");
+kitty.play("laser pointer")
+
+console.log(kitty.favoriteToys);
